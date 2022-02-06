e380c5e30e0fe405d58fa0de8b20179f3ff2f4d3
Daniel Madalitso Phiri <malgamves@gmail.com>
2022-02-06 17:13:34 +0100

    fixed styling, cleaned up files

--- client/components/BlogList.js
+++ client/components/BlogList.js
@@ -18,43 +18,12 @@ const BlogList = ({ allBlogs }) => {
             <div className={styles.grid}>
                 {allBlogs.data.map(post => (
                     <div key={post.attributes.slug}>
-                        {/* <Link href={{ pathname: `/blog/${post.attributes.slug}` }} passHref>
-                            <h2>{post.attributes.title} &rarr; {reformatDate(post.attributes.date)}</h2>
-
-                        </Link> */}
-                        <p><ReactMarkdown
-                            source={truncateSummary(post.attributes.body)}
-                        /></p>
-                        <h2><Link href={{ pathname: `/blog/${post.attributes.slug}` }} passHref>{post.attributes.title}</Link></h2>
-                        <p>{reformatDate(post.attributes.date)}</p>
+                        <h2 className={styles.card}><Link href={{ pathname: `/blog/${post.attributes.slug}` }} passHref>{post.attributes.title}</Link> <p>by {post.attributes.author} on {reformatDate(post.attributes.date)}</p></h2>
+                        <p className={styles.description} >{`${truncateSummary(post.attributes.body)}` + '... (more)'} </p>
                     </div>
-
-                    
-
                 ))}
 
             </div>
-
-            
-                    <h2><Link
-                href="/split"
-                className={styles.card}
-                passHref>Examples &rarr;</Link></h2>
-            
-            
-            <p>Discover and deploy boilerplate example Next.js projects. im trying to see how far thsi line of text goes so here goes nothing</p>
-
-
-            
-                    <h2><Link
-                href="/split"
-                className={styles.card}
-                passHref>Deploy &rarr;</Link></h2>
-            
-            
-            <p>
-                Instantly deploy your Next.js site to a public URL with Vercel. Just making this longer yeah - how long can this go?
-            </p>
 
         </>
     )
