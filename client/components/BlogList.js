10798e8ea040013c04a9ac5915915e39076434de
Daniel Madalitso Phiri <malgamves@gmail.com>
2022-02-11 06:16:41 +0100

    file clean up

--- client/components/BlogList.js
+++ client/components/BlogList.js
@@ -1,7 +1,5 @@
 import Link from 'next/link'
-//import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import ReactMarkdown from 'react-markdown'
 
 const BlogList = ({ allBlogs }) => {
     function truncateSummary(content) {
